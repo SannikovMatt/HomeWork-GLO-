5aa4e17a1fdf0ca931c054c63b6a92cfb43257b9
SannikovMatt <sannikov.matt@gmail.com>
2020-11-23 21:48:32 +0100

    Исправил прием данных

--- Lesson3(extra)/script/script.js
+++ Lesson3(extra)/script/script.js
@@ -3,14 +3,14 @@
 
 let lang,
 
-    temp = confirm('Если нажмете "да" то продолжим на русском ,если "нет" то на английском'),
+    temp = prompt('Выберете на каком языке хотите : en или ru' ,'ru'),
 
     daysRu = 'Понедельник,вторник,среда,четвер,пятница,суббота,воскресение'.toUpperCase(),
 
     daysEn = 'Monday,Tuesday,Wednesday,thursday,friday,saturday,sunday'.toUpperCase();
 
-let ru = [daysRu],
-    en = [daysEn],
+let ru = daysRu.split(','),
+    en = daysEn.split(' ,'),
 
     arr = [en, ru];
 
@@ -18,17 +18,17 @@ let ru = [daysRu],
 
 
 //В зависимости от действий пользователя выбираем язык
-if (temp) {
-    lang = 'ru';
-} else {
-    lang = 'en';
+if (temp === "en") {
+    lang = 0;
+} else if(temp === 'ru') {
+    lang = 1;
 }
 
-if (lang === 'en') {
+if (lang === 0) {
 
     console.log(daysEn);
 
-} else {
+} else if(lang === 1) {
 
     console.log(daysRu);
 }
@@ -37,11 +37,11 @@ if (lang === 'en') {
 
 switch (lang) {
 
-    case 'en':
+    case 0:
         console.log(daysEn);
         break;
 
-    case 'ru':
+    case 1:
         console.log(daysRu);
         break;
 
@@ -49,12 +49,12 @@ switch (lang) {
 
 
 ///через многомерный массив без ифов и switch.
-console.log(arr[+temp][0]);
+console.log(arr[lang].toString());
 
 
 ///__________________________________________2
 
-let namePerson;
+let namePerson=  prompt('Введите одно из имен,чтобы узнать род занятия человека: Артем ;Максим ; Вася' , 'студент' );
 
 namePerson === 'Артем' ? console.log('Директор') : 
 namePerson === 'Максим' ? console.log("преподователь") : console.log("Студент");
