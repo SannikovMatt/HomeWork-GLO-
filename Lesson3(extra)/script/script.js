45e792bbd9e39b57bab16270ccee729a4178521a
SannikovMatt <sannikov.matt@gmail.com>
2020-11-23 20:33:20 +0100

    закончил ДЗ

--- Lesson3(extra)/script/script.js
+++ Lesson3(extra)/script/script.js
@@ -1,25 +1,61 @@
-let num = 266219;
-let numStr = num.toString();//Переводим число в строку
-let sum =0;
+'use strict';
+
+
+let lang,
+
+    temp = confirm('Если нажмете "да" то продолжим на русском ,если "нет" то на английском'),
+
+    daysRu = 'Понедельник,вторник,среда,четвер,пятница,суббота,воскресение'.toUpperCase(),
+
+    daysEn = 'Monday,Tuesday,Wednesday,thursday,friday,saturday,sunday'.toUpperCase();
+
+let ru = [daysRu],
+    en = [daysEn],
+
+    arr = [en, ru];
 
 
 
-for (let i = 0 ; numStr.length > i ; i++)// Перебираем строку умножаем числа.
-{
-    if(sum == 0) sum = +numStr[i];
-    else sum *= +numStr[i];
+
+//В зависимости от действий пользователя выбираем язык
+if (temp) {
+    lang = 'ru';
+} else {
+    lang = 'en';
+}
+
+if (lang === 'en') {
+
+    console.log(daysEn);
+
+} else {
+
+    console.log(daysRu);
+}
+
+
+
+switch (lang) {
+
+    case 'en':
+        console.log(daysEn);
+        break;
+
+    case 'ru':
+        console.log(daysRu);
+        break;
 
 }
 
-console.log(sum);//Полученное число
+
+///через многомерный массив без ифов и switch.
+console.log(arr[+temp][0]);
 
 
-let changedNum = sum ** 3; //Возводим число в 3 степень
-console.log(changedNum);
+///__________________________________________2
 
+let namePerson;
 
-
-changedNum = changedNum.toString();
-
-console.log(changedNum.substring(0,2));//Выводим первые 2 числа
+namePerson === 'Артем' ? console.log('Директор') : 
+namePerson === 'Максим' ? console.log("преподователь") : console.log("Студент");
 
