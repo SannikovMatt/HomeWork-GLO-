b834864f03a52e2ac65d994ce555a763a71fe299
SannikovMatt <sannikov.matt@gmail.com>
2020-12-15 14:17:21 +0100

    Les18e1

---
+++ Lesson18(extra1)/script/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+window.onload= function(){
+
+    const input = document.querySelector('input'),
+        output = document.getElementById('output');
+
+
+    function debounce(func, wait, immediate) {
+        let timeout;
+        return function () {
+            let context = this,
+                args = arguments;
+            let later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            let callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
+
+    input.addEventListener('keypress', debounce(print,1000));
+
+    function print(){
+        output.innerHTML= input.value;
+
+        
+
+    }
+
+}
+
