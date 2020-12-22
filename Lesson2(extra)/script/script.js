ebe147ce02815bbc1a937f62a4db78fc0a9253ae
SannikovMatt <sannikov.matt@gmail.com>
2020-12-22 03:33:34 +0100

    Lesson23_Extra to check

--- Lesson2(extra)/script/script.js
+++ Lesson2(extra)/script/script.js
@@ -1,25 +1,49 @@
-let num = 266219;
-let numStr = num.toString();//Переводим число в строку
-let sum =0;
+'use strict';
+/* 
+
+1.Написать скрипт, которые заменяет слово "функция" и его однокоренные слова в div с id=task1 на «<strong>функция</strong>». 
+
+2. Написать скрипт который в div с id=task2 найдет время. Время имеет формат часы:минуты. И часы, и минуты состоят из двух цифр, пример: 09:00.
+заключить найденное время в тег <b></b>
+
+3. Создать запрос во всем документе найти текст в кавычках и заключить его в теги <mark></mark>
+
+4. Замените в документе домены вида http://site.ru на <a href="http://site.ru">site.ru</a>, 
+
+5. Напишите регулярное выражение для поиска цвета, заданного как #ABCDEF, вывести цвет в консоль
+
+6. Ссылки такого вида http://site.ru/aaaa/bbbb.html заменить
+на <a href="http://site.ru/aaaa/bbbb.html">site.ru</a>
 
 
-
-for (let i = 0 ; numStr.length > i ; i++)// Перебираем строку умножаем числа.
-{
-    if(sum == 0) sum = +numStr[i];
-    else sum *= +numStr[i];
-
-}
-
-console.log(sum);//Полученное число
+Попрактикуйтесь на кроссвордах https://regexcrossword.com/
+и на задачках https://habr.com/ru/post/167015/
+ */
 
 
-let changedNum = sum ** 3; //Возводим число в 3 степень
-console.log(changedNum);
+const task1 = document.getElementById('task1'),
+    body = document.querySelector('body');
 
+task1.innerHTML = task1.innerHTML.replace(/функци(я|и|)/g, (str, val1, val2) => { return str = `<strong>${str}</strong>` });
+let a = [];
 
+let pattern = /([0-9]{2}:[0-9]{2})|(http:\/\/[\-$\/\.a-zA-Z]+)|(#[0-9]{6})|(["«][а-яА-ЯёЁ0-9,.:— ]*["»])/g;
+body.innerHTML = body.innerHTML.replace(pattern, (str, var1, var2, var3, var4) => {
 
-changedNum = changedNum.toString();
+    if (var1) {
 
-console.log(changedNum.substring(0,2));//Выводим первые 2 числа
+        return `<b>${var1}</b>`;
+    } else if (var2) {
 
+        let website = var2.match(/[a-z]+\.ru/g);
+        return `<a href="${var2}">${website[0]}</a>`;
+    } else if (var3) {
+        console.log(var3);
+        return var3;
+    } else if (var4) {
+            var4 = var4.replace(/[0-9]{2}:[0-9]{2}/, (str)=>{ return `<b>${str}</b>`; });
+
+        return `<mark>${var4}</mark>`;
+    }
+
+});
